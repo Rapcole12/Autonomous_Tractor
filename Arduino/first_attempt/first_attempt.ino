a1107c97de886b89c5b50cb150f3c0ce375944bd
RichardMartinez <rmartinez21@vt.edu>
2023-02-02 13:44:32 -0500

    Update first_attempt.ino
    update first_attempt.ino to drive only when button is pressed

--- Arduino/first_attempt/first_attempt.ino
+++ Arduino/first_attempt/first_attempt.ino
@@ -6,7 +6,7 @@
 */
 
 // Program Output:
-// Drive forward for 500 ms, wait 500 ms, drive backward 500 ms
+// Drive forward only when button is pressed
 
 // Definitions Arduino pins connected to input H Bridge
 int IN1 = 4;
@@ -14,6 +14,13 @@ int IN2 = 5;
 int IN3 = 6;
 int IN4 = 7;
 int DELAY = 500; // ms
+int PUSHBUTTON_PIN = 3;
+int PUSHBUTTON = 0;
+
+// Add motor status functions
+int FORWARD = 1;
+int STOP = 0;
+int BACKWARD = -1;
 
 void setup()
 {
@@ -31,6 +38,7 @@ void setup()
     // NOTE: "Clockwise" is from the point of view of the center of the tractor
     // That means that "clockwise" is forward for A, but backwards for B
 
+    /*
     // Turn Motor A Clockwise (Forward)
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
@@ -56,11 +64,61 @@ void setup()
     delay(DELAY);  // Drive backward
     resetPins();
     exit(0);
+    */
 }
 
 void loop()
 {
-  
+  // Drive forward when button is pressed, otherwise stop
+  PUSHBUTTON = digitalRead(PUSHBUTTON_PIN); // Grab HIGH or LOW from button
+  if (PUSHBUTTON == HIGH) {
+    motorA(FORWARD);
+    motorB(FORWARD);
+  }
+  else {
+    motorA(STOP);
+    motorB(STOP);
+  }
+}
+
+void motorA(int status) {
+  if (status == STOP) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+  }
+  else if (status == FORWARD) {
+    // Turn Motor A Clockwise (Forward)
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+  }
+  else if (status == BACKWARD) {
+    // Turn Motor A Counterclockwise (Backward)
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+  }
+  else {
+    return;
+  }
+}
+
+void motorB(int status) {
+  if (status == STOP) {
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  }
+  else if (status == FORWARD) {
+    // Turn Motor B Counterclockwise (Forward)
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }
+  else if (status == BACKWARD) {
+    // Turn Motor B Clockwise (Backward)
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  }
+  else {
+    return;
+  }
 }
 
 void resetPins() {
