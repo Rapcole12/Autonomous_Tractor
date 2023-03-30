08d03c7971f6fb7973481194e8a6a04c6cea6091
Rapcole12 <rapcole12@vt.edu>
2023-03-30 14:12:42 -0400

    Pygame File

---
+++ Python/autonomous_tractor.py
@@ -0,0 +1,37 @@
+#no semicolons
+#all variables are automatically dynamically typed
+#In python there is no curly braces, everything is indentation
+
+import pygame
+
+test1 = True;
+
+test2 = True;
+
+if test1 and test2:
+    print("Hello World")
+
+def myFunction(a, b, c):
+    return a + b + c
+
+""" MATLAB
+for i = 1:5
+
+end
+"""
+
+""" C / C++
+
+for (int i = 0; i < 5; i++) {
+    
+}
+
+"""
+
+# MATLAB: 0:5 == [0, 1, 2, 3, 4, 5]
+
+# Python: range(0, 5) == [0, 1, 2, 3, 4]
+
+    
+        
+
