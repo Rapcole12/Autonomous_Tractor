34d7dd824b974e2d36800236b0750f1f2dc831b8
Rapcole12 <rapcole12@vt.edu>
2023-04-03 18:53:44 -0400

    While loop for whether BLE is connected

--- Python/autonomous_tractor_BLE.py
+++ Python/autonomous_tractor_BLE.py
@@ -1,11 +1,13 @@
 import asyncio
 from bleak import BleakClient
 
+BLE_ADDRESS = "34:08:E1:19:51:E7"
+
 async def main():
-    async with BleakClient("34:08:E1:19:51:E7") as client:
-        if client.is_connected:
+    async with BleakClient(BLE_ADDRESS) as client:
+        while client.is_connected:
            print("BLE BLUETOOTH IS CONNECTED")
-        else:
-            print("BLE BLUETOOTH IS NOT CONNECTED")
+        
+    print("BLE BLUETOOTH IS NOT CONNECTED")
 
 asyncio.run(main())
