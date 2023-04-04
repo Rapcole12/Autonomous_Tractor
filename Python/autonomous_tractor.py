d281d560e9ccc67fb37bd124b06b35e6cbd0c411
RichardMartinez <rmartinez21@vt.edu>
2023-04-03 20:16:07 -0400

    add todos for pygame

--- Python/autonomous_tractor.py
+++ Python/autonomous_tractor.py
@@ -13,6 +13,21 @@ window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 running = True
 color = "red"
 
+# TODO: Every time the enter key is pressed, print "ENTER" to the console, set screen to "blue"
+# TODO: Every time the backspace key is pressed, print "BACKSPACE" to the console, set screen to "yellow"
+# https://www.pygame.org/docs/ref/key.html
+# Use if statements similar to event.key == pygame.K_SPACE
+
+# TODO: Display some text on the screen (Doesn't really matter what text)
+# https://www.geeksforgeeks.org/python-display-text-to-pygame-window/#
+# https://www.pygame.org/docs/ref/font.html#pygame.font.Font.render   (Read this whole page really)
+# https://www.pygame.org/docs/ref/font.html#pygame.font.SysFont
+# Basically, create a SysFont object (no need for a .ttf file) and render text to a surface
+# Then blit that surface to the screen (blit means drawing one surface onto another in pygame)
+
+# TODO: Try drawing an image on the screen (maybe like a tractor stock image)
+# https://www.pygame.org/docs/ref/image.html
+
 # Main Loop
 while running:
     for event in pygame.event.get():
