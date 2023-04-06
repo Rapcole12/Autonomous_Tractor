83b7df147a65316a0d6e3a4a078b09390af17526
Rapcole12 <rapcole12@vt.edu>
2023-04-06 12:44:55 -0400

    Put the code worked on tuesday in a new file

--- Python/autonomous_tractor.py
+++ Python/autonomous_tractor.py
@@ -1,5 +1,7 @@
-# Import Pygame
+# Import Pygame and Bluetooth
+import asyncio
 import pygame
+from bleak import BleakClient
 
 # Init Pygame
 pygame.init()
@@ -9,6 +11,13 @@ WINDOW_HEIGHT = 500
 WINDOW_WIDTH = 800
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
+#set BLE Parameters
+BLE_ADDRESS = "34:08:E1:19:51:E7"
+BLE_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
+
+#Set ON and OFF commands
+ON = "ON\n".encode("ASCII")
+OFF = "OFF\n".encode("ASCII")
 
 # Is the GUI running?
 running = True
@@ -17,6 +26,9 @@ color = "red"
 ##SET THE TEXT
 white = (255,255,255)
 black = (0,0,0)
+
+##Counter to display a message once
+display_once = 0
 
 font = pygame.font.SysFont("timesnewroman", 20)
 
@@ -31,46 +43,43 @@ textRect.center = (WINDOW_WIDTH // 4, WINDOW_HEIGHT // 2)
 ##SET THE IMAGE
 image_extration = pygame.image.load("Tractor.png")
 
-# TODO: Every time the enter key is pressed, print "ENTER" to the console, set screen to "blue"
-# DONE
-# TODO: Every time the backspace key is pressed, print "BACKSPACE" to the console, set screen to "yellow" 
-# DONE 
-# https://www.pygame.org/docs/ref/key.html
-# Use if statements similar to event.key == pygame.K_SPACE
-
-# TODO: Display some text on the screen (Doesn't really matter what text)
-# https://www.geeksforgeeks.org/python-display-text-to-pygame-window/#
-# https://www.pygame.org/docs/ref/font.html#pygame.font.Font.render   (Read this whole page really)
-# https://www.pygame.org/docs/ref/font.html#pygame.font.SysFont
-# Basically, create a SysFont object (no need for a .ttf file) and render text to a surface
-# Then blit that surface to the screen (blit means drawing one surface onto another in pygame)
-
-# TODO: Try drawing an image on the screen (maybe like a tractor stock image)
-# https://www.pygame.org/docs/ref/image.html
-
-# Main Loop
-while running:
-    #This is for user inputs
+def handle_Motors(client):
+    global running, color
+        #This is for user inputs
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-        if event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 color = "green"
-            if event.key == pygame.K_RETURN: 
+            elif event.key == pygame.K_RETURN: 
+                client.write_gatt_char(BLE_UUID, ON)
                 color = "blue"
                 print("PRESSED ENTER")
-            if event.key == pygame.K_BACKSPACE:
+            elif event.key == pygame.K_BACKSPACE:
+                client.write_gatt_char(BLE_UUID, OFF)
                 color = "yellow"
-                print("PRESSED BACKSPACE")
-                 
+                print("PRESSED BACKSPACE")     
     window.fill(color)
-    #Start drawing Strings HERE
+            #Start drawing Strings HERE
     window.blit(text,(360,50)) ## LOWER Y COORDINATE MEANS GO UP and LOWER X COORDINATE MEANS GO LEFT
     window.blit(image_extration, (275,250))
-
     pygame.display.update()
 
-pygame.quit()
+async def Handle_BLE_Connection(client):
+   if client.is_connected:
+      print("BLE IS CONNECTED")
+      display_once = display_once + 1
+
+async def main():
+    # Main Loop
+    async with BleakClient(BLE_ADDRESS) as client:
+         while running:
+            if display_once == 0:
+                Handle_BLE_Connection(client)    
+            handle_Motors(client)
+    pygame.quit()
+
+asyncio.run(main())
+
     
