e956a124e48e50d7a98e92a3ee0194fe6cb6814a
RichardMartinez <rmartinez21@vt.edu>
2023-04-11 15:29:01 -0400

    running BLE while loop and pygame at same time

--- Python/autonomous_tractor.py
+++ Python/autonomous_tractor.py
@@ -1,21 +1,28 @@
+# Import Bleak
+import asyncio
+from bleak import BleakClient
+
+# BLE Parameters
+BLE_ADDRESS = "34:08:E1:19:51:E7"
+BLE_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
+
+# ASCII Strings to Send
+ON = "ON\n".encode("ASCII")
+OFF = "OFF\n".encode("ASCII")
+
 # Import Pygame
 from cmath import rect
 from operator import length_hint
 import pygame
 
-# Init Pygame
-pygame.init()
+WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 800
 
 # Colors 
 white = (255,255,255)
 black = (0,0,0)
 light_blue = (173,216,230)
 light_green = (144,238,144)
-
-# Set Window
-WINDOW_HEIGHT = 500
-WINDOW_WIDTH = 800
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 # Set Grass 
 Rec_Width = 800
@@ -39,13 +46,11 @@ Tractor_rect = Tractor.get_rect()
 Tractor_rect.center = (150, 335)
 
 # SET THE TEXT
-font = pygame.font.SysFont("timesnewroman", 20)
-text =  font.render('ECE 2804', True, black, white)
+#font = pygame.font.SysFont("timesnewroman", 20)
+#text =  font.render('ECE 2804', True, black, white)
 
-"""
-textRect = text.get_rect()
-textRect.center = (WINDOW_WIDTH // 4, WINDOW_HEIGHT // 2)
-"""
+#textRect = text.get_rect()
+#textRect.center = (WINDOW_WIDTH // 4, WINDOW_HEIGHT // 2)
 
 # Is the GUI running?
 running = True
@@ -57,7 +62,7 @@ def pygame_mainloop():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        """
+        
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 color = "green"
@@ -67,12 +72,12 @@ def pygame_mainloop():
             if event.key == pygame.K_BACKSPACE:
                 color = "yellow"
                 print("PRESSED BACKSPACE")
-        """     
+            
     #Start drawing Strings HERE
-    """
-    window.blit(text,(360,50)) ## LOWER Y COORDINATE MEANS GO UP and LOWER X COORDINATE MEANS GO LEFT
-    window.blit(image_extration, (275,250))
-    """
+        
+    #window.blit(text,(360,50)) ## LOWER Y COORDINATE MEANS GO UP and LOWER X COORDINATE MEANS GO LEFT
+    #window.blit(image_extration, (275,250))
+    
     window.fill(light_blue)
     pygame.draw.rect(window,light_green ,rect)
     pygame.draw.ellipse(window,white, Elipse_cloud_1)
@@ -83,10 +88,25 @@ def pygame_mainloop():
     window.blit(Tractor,Tractor_rect)
     pygame.display.update()
 
-# Main Loop
-while running:
-    pygame_mainloop()
 
+# Main Program
+async def main():
+    async with BleakClient(BLE_ADDRESS) as client:
+        # Init Pygame
+        pygame.init()
+
+        # Set Window
+        global window, running
+        window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+        while (running):
+            print("INSIDE BLE")
+            pygame_mainloop()
+
+# Run the program
+asyncio.run(main())
+
+# Quit when done
 pygame.quit()
     
 
