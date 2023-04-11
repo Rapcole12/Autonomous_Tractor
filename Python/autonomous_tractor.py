113d8576429cbd6d42a730304d6e7dc96e898034
RichardMartinez <rmartinez21@vt.edu>
2023-04-11 14:57:58 -0400

    move to pygame_mainloop

--- Python/autonomous_tractor.py
+++ Python/autonomous_tractor.py
@@ -5,7 +5,8 @@ import pygame
 
 # Init Pygame
 pygame.init()
-##Colors 
+
+# Colors 
 white = (255,255,255)
 black = (0,0,0)
 light_blue = (173,216,230)
@@ -16,12 +17,12 @@ WINDOW_HEIGHT = 500
 WINDOW_WIDTH = 800
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-#Set Grass 
+# Set Grass 
 Rec_Width = 800
 Rec_Height = 50
 rect = pygame.Rect(0,WINDOW_HEIGHT-Rec_Height,Rec_Width,Rec_Height)
 
-##Set the Clouds 
+# Set the Clouds 
 Elipse_width = 125
 Elipse_height = 50
 Elipse_cloud_1 = pygame.Rect(35, 60, Elipse_width, Elipse_height)
@@ -30,33 +31,29 @@ Elipse_cloud_3 = pygame.Rect(365, 35, Elipse_width, Elipse_height)
 Elipse_cloud_4 = pygame.Rect(500, 110, Elipse_width, Elipse_height)
 Elipse_cloud_5 = pygame.Rect(650, 10, Elipse_width, Elipse_height)
 
-
-##SET Tractor 
+# Set Tractor Image
 Tractor = pygame.image.load('Tractor_transparent.png');
 Tractor_rect = Tractor.get_rect()
 Tractor = pygame.transform.scale(Tractor, (Tractor_rect.width // 2, Tractor_rect.height // 2))
 Tractor_rect = Tractor.get_rect()
 Tractor_rect.center = (150, 335)
-# Is the GUI running?
-running = True
 
-##SET THE TEXT
-
+# SET THE TEXT
 font = pygame.font.SysFont("timesnewroman", 20)
-
 text =  font.render('ECE 2804', True, black, white)
 
 """
 textRect = text.get_rect()
 textRect.center = (WINDOW_WIDTH // 4, WINDOW_HEIGHT // 2)
 """
- 
 
+# Is the GUI running?
+running = True
 
-# Main Loop
-while running:
+def pygame_mainloop():
+    global running
+
     #This is for user inputs
-   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -85,6 +82,10 @@ while running:
     pygame.draw.ellipse(window, white, Elipse_cloud_5)
     window.blit(Tractor,Tractor_rect)
     pygame.display.update()
+
+# Main Loop
+while running:
+    pygame_mainloop()
 
 pygame.quit()
     
